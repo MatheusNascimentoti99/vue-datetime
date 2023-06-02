3dcb173639f9fa8d6b421d4327fa9cf1b91667eb
adamPrestor <adam.prestor@gmail.com>
2023-06-02 14:10:43 +0200

    Remove legacy unittests

--- packages/lib/test/legacy/helpers/index.js
+++
@@ -1,8 +0,0 @@
-import { createVM, Vue } from 'packages/lib/test/legacy/helpers/utils'
-import { nextTick } from 'packages/lib/test/legacy/helpers/wait-for-update'
-
-export {
-  createVM,
-  Vue,
-  nextTick
-}
